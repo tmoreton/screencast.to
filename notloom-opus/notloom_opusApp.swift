25ebe592c1ca9efc2983a4a801cbf3365b6baaa3
Tim Moreton <tmoreton89@gmail.com>
2026-05-11 12:52:57 -0400

    Initial Commit

---
+++ notloom-opus/notloom_opusApp.swift
@@ -0,0 +1,17 @@
+//
+//  notloom_opusApp.swift
+//  notloom-opus
+//
+//  Created by Tim Moreton on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct notloom_opusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
